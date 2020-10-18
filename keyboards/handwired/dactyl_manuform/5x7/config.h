852d824d70394743decef450636322dc2442ac23
Bruce Jones <bruce.david.jones@gmail.com>
2020-10-17 20:44:57 -0400

    I2C, mirrored wiring

--- keyboards/handwired/dactyl_manuform/5x7/config.h
+++ keyboards/handwired/dactyl_manuform/5x7/config.h
@@ -20,9 +20,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "config_common.h"
 
-#define PRODUCT_ID 0x3537
-#define DEVICE_VER 0x0001
-#define PRODUCT    Dactyl-Manuform (5x7)
+#define PRODUCT_ID      0x3537
+#define DEVICE_VER      0x0001
+#define PRODUCT         Dactyl-Manuform (5x7)
+#define USE_I2C
+#define SPLIT_USB_DETECT
 
 /* key matrix size */
 // Rows are doubled-up
@@ -32,6 +34,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // wiring of each half
 #define MATRIX_ROW_PINS { D4, C6, D7, E6, B4, B5 }
 #define MATRIX_COL_PINS { F5, F6, F7, B1, B3, B2, B6 }
+#define MATRIX_COL_PINS_RIGHT { B6, B2, B3, B1, F7, F6, F5 }
 
 #define DIODE_DIRECTION COL2ROW
 
